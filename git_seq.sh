7b5f78ab4f602f5ba917b8750601dcb7a92e3fd3
seojikim <seojikim@fadutec.com>
2022-06-09 01:17:13 +0900

    [subgit] add svn multiple user commit test script
    Signed-off-by: seojikim <seojikim@fadutec.com>

--- git_seq.sh
+++ git_seq.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
 
 PWD=`pwd`
-GIT_REPO_DIR=/home/seojikim/work/ci_cd/testtest
+LOGFILE=git_seq.log
+#GIT_REPO_DIR=/home/seojikim/work/ci_cd/testtest
 #GIT_REPO_DIR=/home/seojikim/work/ci_cd/test_repo
-SEQ_NUM=50
+#SEQ_NUM=50
 
 function git_sequential_commit_1user_diff_file() {
 	cd $GIT_REPO_DIR
 	echo "[GIT] sequential commit test - 1 user"
+	echo "--------------------------------------------------"
 	
-	if [ ! -d git_sequential ]; then
+	if [ ! -d git_sequential3 ]; then
 		echo "mkdir git_sequential"
-		mkdir git_sequential
+		mkdir git_sequential3
 	fi
 
 	for ((i=1;i<$SEQ_NUM+1;i++));
 	do
-		echo $i
+		sleep 5
+		echo "[GIT] sequential commit test $i - 1 user - diff file"
+		echo "--------------------------------------------------"
 		git pull --rebase  origin master
-		touch git_sequential/testfile$i
-		git add git_sequential/testfile$i
+		touch git_sequential3/testfile$i
+		git add git_sequential3/testfile$i
 		git commit -m "[git] sequential commit test $i"
 		git push origin master
 	done
+	echo "--------------------------------------------------"
 	
 	cd $PWD
 }
@@ -30,20 +35,43 @@ function git_sequential_commit_1user_diff_file() {
 function git_sequential_commit_1user_same_file() {
 	cd $GIT_REPO_DIR
 	echo "[GIT] sequential commit test - 1 user - same file"
+	echo "--------------------------------------------------"
 	
 	for ((i=1;i<$SEQ_NUM+1;i++));
 	do
-		sleep 240
-		echo $i
+		#sleep 5
+		echo "[GIT push test $i]"
+		echo "--------------------------------------------------"
 		git pull --rebase origin master
 		echo "add by git - $i" >> git_svn_sequential/testfile
 		git add git_svn_sequential/testfile
 		git commit -m "[git] sequential commit test $i - 1 user - same file"
 		git push origin master
 	done
+	echo "--------------------------------------------------"
 	
 	cd $PWD
 }
 
-#git_sequential_commit_1user_diff_file
+function usage() {
+	echo "Usage: $0 -a {GIT_REPO_DIR} -c {CNT}"
+}
+
+if [ $# -lt 2 ]; then
+	usage
+	exit 1
+fi
+while getopts ":a:c:" opt; do
+	case $opt in
+		a) GIT_REPO_DIR="$OPTARG"
+		;;
+		c) SEQ_NUM="$OPTARG"
+		;;
+		*) usage
+		exit 1
+		;;
+	esac
+done
+
+#git_sequential_commit_1user_diff_file | tee -a ${LOGFILE}
 git_sequential_commit_1user_same_file
--- svn_seq.sh
+++ svn_seq.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
 PWD=`pwd`
-SVN_REPO_DIR=/user/seojikim/work/svn_repo
+#SVN_REPO_DIR=/user/seojikim/work/svn_repo
 #SVN_REPO_DIR=/user/seojikim/work/test_repo
-SVN_USER=fadu1
-SVN_PW=fadu1
-SEQ_NUM=50
+#SVN_USER=fadu1
+#SVN_PW=fadu1
+#SEQ_NUM=50
 
 function svn_sequential_commit_1user_diff_file() {
 	cd $SVN_REPO_DIR
 	echo "[SVN] sequential commit test - 1 user"
+	echo "--------------------------------------------------"
+	svn up
 	
-	if [ ! -d trunk/svn_sequential ]; then
+	if [ ! -d trunk/svn_sequential3 ]; then
 		echo "mkdir svn_sequential"
 		svn up
-		mkdir -p trunk/svn_sequential
-		svn add trunk/svn_sequential
+		mkdir -p trunk/svn_sequential3
+		svn add trunk/svn_sequential3
 		svn commit --username $SVN_USER --password $SVN_PW -m "[svn] add svn_sequential"
 	fi
 
 	for ((i=1;i<$SEQ_NUM+1;i++));
 	do
-		echo $i
+		sleep 5
+		echo "[SVN] sequential commit test $i - 1 user - diff file"
+		echo "--------------------------------------------------"
 		svn up
-		touch trunk/svn_sequential/testfile$i
-		svn add trunk/svn_sequential/testfile$i
+		touch trunk/svn_sequential3/testfile$i
+		svn add trunk/svn_sequential3/testfile$i
 		svn commit --username $SVN_USER --password $SVN_PW -m "[svn] sequential commit test $i"
 	done
 	
@@ -34,19 +38,50 @@ function svn_sequential_commit_1user_diff_file() {
 function svn_sequential_commit_1user_same_file() {
 	cd $SVN_REPO_DIR
 	echo "[SVN] sequential commit test - 1 user - same file"
+	echo "--------------------------------------------------"
 
 	for ((i=1;i<$SEQ_NUM+1;i++));
 	do
-		sleep 240
-		echo $i
+		#sleep 5
+		echo [SVN commit test i]
+		echo "--------------------------------------------------"
 		svn up
 		echo "add by svn - $i" >> trunk/git_svn_sequential/testfile
 		svn add trunk/git_svn_sequential/testfile
 		svn commit --username $SVN_USER --password $SVN_PW -m "[svn] sequential commit test $i - 1 user - same file"
 	done
+	echo "--------------------------------------------------"
 	
 	cd $PWD
 }
+
+function usage() {
+	echo "Usage: $0 -a {GIT_REPO_DIR} -c {CNT} -u {SVN_USER} -p {SVN_PW}"
+}
+
+if [ $# -lt 2 ]; then
+	usage
+	exit 1
+fi
+while getopts ":a:u:p:c:" opt; do
+	case $opt in
+		a) SVN_REPO_DIR="$OPTARG"
+		;;
+		u) SVN_USER="$OPTARG"
+		;;
+		p) SVN_PW="$OPTARG"
+		;;
+		c) SEQ_NUM="$OPTARG"
+		;;
+		*) usage
+		exit 1
+		;;
+	esac
+done
+echo $SVN_REPO_DIR
+echo $SVN_USER
+echo $SVN_PW
+echo $SEQ_NUM
 
 #svn_sequential_commit_1user_diff_file
 svn_sequential_commit_1user_same_file
